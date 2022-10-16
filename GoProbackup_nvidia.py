a4f79f7046846446647ada1ec4dc1f70aac969aa
Dongho Park <asulit@gmail.com>
2022-10-17 02:17:39 +0900

    change encoding options

--- GoProbackup_nvidia.py
+++ GoProbackup_nvidia.py
@@ -6,14 +6,20 @@ VIDEO_BITRATE = '20M'
 
 
 def encode_h265(filename):
-    cmd = ('ffmpeg -y -vsync 0 -hwaccel cuda '
+    cmd = ('ffmpeg -y -hwaccel cuda '
           '-i ' + GOPRO_PATH + '/' + filename + ' '
-          '-c:a copy -c:v hevc_nvenc -profile:v 2 -b:v ' + VIDEO_BITRATE + ' '
-          '-map_metadata 0 '
+          # audio codec
+          '-c:a copy ' 
+          # video codec
+          '-c:v hevc_nvenc -profile:v main10 -b:v ' + VIDEO_BITRATE + ' '  
+          '-map_metadata 0 ' # meta data
           '' + BACKUP_PATH + '/' + filename + ''
            )
+    print(cmd)
     os.system(cmd)
 
 for each in os.listdir(GOPRO_PATH):
 	if each.endswith(".MP4"):		
 		encode_h265(each)
+
+# encode_h265("GX010152.MP4")
