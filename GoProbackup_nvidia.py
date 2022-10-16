2131d4b6e957e3ea9c82c3fbda8bd7612334eac7
Dongho Park <asulit@gmail.com>
2022-10-17 01:54:07 +0900

    copy metadata

--- GoProbackup_nvidia.py
+++ GoProbackup_nvidia.py
@@ -4,15 +4,16 @@ GOPRO_PATH = 'J:/DCIM/100GOPRO'
 BACKUP_PATH = 'D:/Picture/GoPro/Output2'
 VIDEO_BITRATE = '20M'
 
-def encodeH265(filename):
-	cmd = ('ffmpeg -y -vsync 0 -hwaccel cuda '
-		'-i ' + GOPRO_PATH + '/' + filename + ' '
-		'-movflags use_metadata_tags '
-		'-c:a copy -c:v hevc_nvenc -profile:v 2 -b:v ' + VIDEO_BITRATE + ' '
-		'' + BACKUP_PATH + '/' + filename + ''
-	) 
-	os.system(cmd)
+
+def encode_h265(filename):
+    cmd = ('ffmpeg -y -vsync 0 -hwaccel cuda '
+          '-i ' + GOPRO_PATH + '/' + filename + ' '
+          '-c:a copy -c:v hevc_nvenc -profile:v 2 -b:v ' + VIDEO_BITRATE + ' '
+          '-map_metadata 0 '
+          '' + BACKUP_PATH + '/' + filename + ''
+           )
+    os.system(cmd)
 
 for each in os.listdir(GOPRO_PATH):
 	if each.endswith(".MP4"):		
-		encodeH265(each)
+		encode_h265(each)
