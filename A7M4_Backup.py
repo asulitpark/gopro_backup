df2f0193c6fce41884798dfadec53cf14b21c66e
Dongho Park <asulit@gmail.com>
2023-05-23 00:38:47 +0900

    add a7m4 with stabilization

--- A7M4_Backup.py
+++ A7M4_Backup.py
@@ -1,28 +1,40 @@
 import os
 import win32file
+from pywintypes import Time
 
-GOPRO_PATH = 'J:/DCIM/100GOPRO'
-BACKUP_PATH = 'Y:/GoPro'
+A7M4_PATH = 'Y:/test from'
+BACKUP_PATH = 'Y:/test to'
 VIDEO_BITRATE = '20M'
 
-
-def encode_h265(filename):
+def encode_h265(fromFile, toFile):
     cmd = ('ffmpeg -y -hwaccel cuda '
-          '-i ' + GOPRO_PATH + '/' + filename + ' '
-          # audio codec
-          '-c:a copy ' 
-          # video codec
+          '-i "' + fromFile + '" '
+          '-vf vidstabdetect=shakiness=10 '
           '-c:v hevc_nvenc -profile:v main10 -b:v ' + VIDEO_BITRATE + ' '  
-          # meta data
-          '-map_metadata 0 '
-          '' + BACKUP_PATH + '/' + filename + ''
+          '"' + toFile + '"'
            )
     print(cmd)
     os.system(cmd)
-    os.path.
 
-for each in os.listdir(GOPRO_PATH):
-	if each.endswith(".MP4"):		
-		encode_h265(each)
+    cmd = ('ffmpeg -y -hwaccel cuda '
+          '-i "' + fromFile + '" '
+          '-vf vidstabtransform,unsharp=5:5:0.8:3:3:0.4 '
+          '-c:v hevc_nvenc -profile:v main10 -b:v ' + VIDEO_BITRATE + ' '  
+          '"' + toFile + '"'
+           )
+    print(cmd)
+    os.system(cmd)
+
+def overwriteCreateTime(fromFile, toFile):
+	fromDate = os.path.getmtime(fromFile)
+	os.utime(toFile, (fromDate, fromDate))
+
+for each in os.listdir(A7M4_PATH):
+	if each.endswith(".MP4"):
+		fromFile = '' + A7M4_PATH + '/' + each
+		toFile = '' + BACKUP_PATH + '/' + each
+		encode_h265(fromFile, toFile)
+		overwriteCreateTime(fromFile, toFile)
+        
 
 # encode_h265("GX010152.MP4")
--- GoProbackup_nvidia.py
+++ GoProbackup_nvidia.py
@@ -5,22 +5,29 @@ BACKUP_PATH = 'Y:/GoPro'
 VIDEO_BITRATE = '20M'
 
 
-def encode_h265(filename):
+def encode_h265(fromFile, toFile):
     cmd = ('ffmpeg -y -hwaccel cuda '
-          '-i ' + GOPRO_PATH + '/' + filename + ' '
+          '-i "' + fromFile + '" '
           # audio codec
           '-c:a copy ' 
           # video codec
           '-c:v hevc_nvenc -profile:v main10 -b:v ' + VIDEO_BITRATE + ' '  
           # meta data
           '-map_metadata 0 '
-          '' + BACKUP_PATH + '/' + filename + ''
+          '"' + toFile + '"'
            )
     print(cmd)
     os.system(cmd)
 
+def overwriteCreateTime(fromFile, toFile):
+	fromDate = os.path.getmtime(fromFile)
+	os.utime(toFile, (fromDate, fromDate))
+	
 for each in os.listdir(GOPRO_PATH):
-	if each.endswith(".MP4"):		
-		encode_h265(each)
+	if each.endswith(".MP4"):
+		fromFile = '' + GOPRO_PATH + '/' + each
+		toFile = '' + BACKUP_PATH + '/' + each
+		encode_h265(fromFile, toFile)
+		overwriteCreateTime(fromFile, toFile)
 
 # encode_h265("GX010152.MP4")
